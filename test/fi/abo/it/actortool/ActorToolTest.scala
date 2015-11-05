6228b93532021b45668c9ea6e2c74bcdb63b897e
Jonatan Wiik <jonatan.wiik@abo.fi>
2015-11-05 11:06:33 +0200

    Added libraries and folders for unit tests.

---
+++ test/fi/abo/it/actortool/ActorToolTest.scala
@@ -0,0 +1,31 @@
+package fi.abo.it.actortool
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import java.io.File
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ActorToolTestSuite extends FunSuite {
+  
+  test("Schedule.actor") {
+    val params = new ActorTool.CommandLineParameters{
+        val BoogiePath = "./boogie"
+        val Files = List(new File("tests/Schedule.actor"))
+        val BoogieArgs = ""
+        val PrintProgram = true
+        val DoTypecheck = true
+        val DoInfer = true
+        val DoTranslate = true
+        val DoVerify = true
+        val NoBplFile = true
+        val BplFile = "out.bpl"
+        val Timing = 2
+    }
+    
+    ActorTool.verify(params)
+  }
+  
+  
+}
